903d2f7dacd5b137dd192d95b3e44485f23e4c61
Tajamul <thetj4054@gmail.com>
2024-06-23 21:44:32 +0530

    Updated the projects and added new styles

--- index.js
+++ index.js
@@ -56,8 +56,7 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading ', { origin : "top"});
-ScrollReveal().reveal('.home-img, .skills-container, .contact-media', { origin : "bottom"});
-// ScrollReveal().reveal('.home-content h1, .about-img', { origin : "left"});
-// ScrollReveal().reveal('.home-content p, .about-content ', { origin : "right"});
+ScrollReveal().reveal('.home-img, .skills-container', { origin : "bottom"});
+
 
 
